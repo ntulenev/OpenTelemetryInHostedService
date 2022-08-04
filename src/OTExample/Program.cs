ef7accb7287d2a26b19153905d3fae6c647b995d
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-04 19:23:24 +0300

    Rergister metrics

--- src/OTExample/Program.cs
+++ src/OTExample/Program.cs
@@ -1,6 +1,8 @@
+using OTExample.Metrics;
 using OTExample.Service;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<IMetricsUtil, MetricsUtil>();
 builder.Services.AddHostedService<SomeService>();
 var app = builder.Build();
 await app.RunAsync();
