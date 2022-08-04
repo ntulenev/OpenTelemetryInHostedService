e2a1cdb7e537d17c22cec8b4bea7665891f335dc
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-04 19:23:35 +0300

    Add metrics in service

--- src/OTExample/Service/SomeService.cs
+++ src/OTExample/Service/SomeService.cs
@@ -1,17 +1,27 @@
-﻿namespace OTExample.Service
+﻿using OTExample.Metrics;
+
+namespace OTExample.Service
 {
     public class SomeService : BackgroundService
     {
+        public SomeService(IMetricsUtil metrics)
+        {
+            _metrics = metrics ?? throw new ArgumentNullException(nameof(metrics));
+        }
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (true)
             {
                 stoppingToken.ThrowIfCancellationRequested();
 
+                using var _ = _metrics.MeasureTime();
+
                 await Task.Delay(_rnd.Next(1000, 5000), stoppingToken); // Some work
             }
         }
 
+        private readonly IMetricsUtil _metrics;
         private readonly Random _rnd = new();
     }
 }
