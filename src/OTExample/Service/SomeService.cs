343d900b998c2641389b11e4900c48deba1aa8a7
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-04 19:11:18 +0300

    Add target service

---
+++ src/OTExample/Service/SomeService.cs
@@ -0,0 +1,17 @@
+﻿namespace OTExample.Service
+{
+    public class SomeService : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (true)
+            {
+                stoppingToken.ThrowIfCancellationRequested();
+
+                await Task.Delay(_rnd.Next(1000, 5000), stoppingToken); // Some work
+            }
+        }
+
+        private readonly Random _rnd = new();
+    }
+}
