bd3585c4dac24e9c04ef07f84e034746e3fcc65d
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-04 19:23:17 +0300

    Add metrics stub implementation

---
+++ src/OTExample/Metrics/MetricsUtil.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace OTExample.Metrics
+{
+    public class MetricsUtil : IMetricsUtil
+    {
+        public IDisposable MeasureTime([CallerMemberName] string name = "")
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
