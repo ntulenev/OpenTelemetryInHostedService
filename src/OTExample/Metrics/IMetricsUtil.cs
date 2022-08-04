05513a5cac989c499e47ef5a8d97cc270d41d993
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-04 19:23:06 +0300

    Add metrics contract

---
+++ src/OTExample/Metrics/IMetricsUtil.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace OTExample.Metrics
+{
+    public interface IMetricsUtil
+    {
+        public IDisposable MeasureTime([CallerMemberName] string name = "");
+    }
+}
