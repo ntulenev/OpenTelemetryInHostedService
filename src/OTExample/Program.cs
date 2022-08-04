f0e72952d8262305fd5c7cf0f398a70680a74e9e
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-04 19:56:54 +0300

    Init ConsoleMetricsProvider

--- src/OTExample/Program.cs
+++ src/OTExample/Program.cs
@@ -1,8 +1,11 @@
 using OTExample.Metrics;
 using OTExample.Service;
 
+using var _ = ConsoleMetricsProvider.Init();
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IMetricsUtil, MetricsUtil>();
 builder.Services.AddHostedService<SomeService>();
+
 var app = builder.Build();
 await app.RunAsync();
