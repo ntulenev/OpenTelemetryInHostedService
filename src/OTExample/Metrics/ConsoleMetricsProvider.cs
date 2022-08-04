1f2e949997f58f236995ff700a18d5c68a16f1f9
Nikita Tulenev <n1tulenev@gmail.com>
2022-08-04 19:56:25 +0300

    Add ConsoleMetricsProvider

---
+++ src/OTExample/Metrics/ConsoleMetricsProvider.cs
@@ -0,0 +1,20 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace OTExample.Metrics
+{
+    public static class ConsoleMetricsProvider
+    {
+        public static IDisposable Init()
+        {
+            return Sdk.CreateTracerProviderBuilder()
+           .AddSource(MetricsConstantcs.SERVICE_NAME)
+           .SetResourceBuilder(
+                ResourceBuilder.CreateDefault()
+               .AddService(serviceName: MetricsConstantcs.SERVICE_NAME))
+           .AddConsoleExporter()
+           .Build();
+        }
+    }
+}
